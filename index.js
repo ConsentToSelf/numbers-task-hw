ad24805041f6943f734f88c3a1366bc65e165058
Султан <carameleclipsimus@gmail.com>
2023-04-15 19:21:35 +0600

    feat: done first task udemy

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+// first
+let getPi = Math.PI;
+console.log(+getPi.toFixed(2));
+// second
+console.log(Math.min(15, 11, 16, 12, 51, 12, 13, 51));
+console.log(Math.max(15, 11, 16, 12, 51, 12, 13, 51));
+// third
+let num;
+console.log(Math.random(num).toFixed(2));
+console.log(Math.round(Math.random(num) * 10));
+// fourth
+console.log((0.6 + 0.7).toFixed(1));
+// Five
+console.log(parseInt("100$"));
